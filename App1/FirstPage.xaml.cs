e16817f88c60a07301a22e4d0621d23be73e50be
karinakorpusenko <157098151+karinakorpusenko@users.noreply.github.com>
2024-12-18 03:25:12 +0500

    Add files via upload

---
+++ App1/FirstPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnNavigateToSecondPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
